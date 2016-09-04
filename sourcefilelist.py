b6c22a6875ca86a654f4f86fe8220ef43e4025da
Thomas H <tom8941@users.noreply.github.com>
2016-09-04 09:24:39 +0200

    Update sourcefilelist.py

--- sourcefilelist.py
+++ sourcefilelist.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# MISP-IOC-Validator - Validate IOC from MISP ; Export results and iocs to SIEM and sensors using syslog and CEF format
+#
+# Copyright (C) 2016 Thomas Hilt
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 tld_list_filepath = 'datasrc/tlds-alpha-by-domain.txt'          #list of existing TLD
 google_ip_list_filepath = 'datasrc/google_ip.txt'               #ip address or range of google CIDR notation
 yahoo_ip_list_filepath = 'datasrc/yahoo_ip.txt'                 #ip address or range of yahoo CIDR notation
