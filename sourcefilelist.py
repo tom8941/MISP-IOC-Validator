a3e144dd77f86c3369d7f62ca66540ebb673c2ca
Thomas Hilt <tom8941@users.noreply.github.com>
2016-09-06 10:34:29 +0200

    Update sourcefilelist.py

--- sourcefilelist.py
+++ sourcefilelist.py
@@ -24,6 +24,7 @@ yahoo_ip_list_filepath = 'datasrc/yahoo_ip.txt'                 #ip address or r
 microsoft_ip_list_filepath = 'datasrc/microsoft_ip.txt'         #ip address or range of microsoft CIDR notation
 alexa_list_filepath = 'datasrc/alexalist.txt'                   #alexa top 1M list (without the first line of csv file)
 alexa_exception_list_filepath = 'datasrc/alexa_exception.txt'   #some domain that we want to exclude from alexa list
+yara_export_exception_list_filepath = 'datasrc/yara-export-discarded.txt' #contain a list of name of yara rule (one per line) to exclude from the yara export function
 md5_list_filepath = 'datasrc/md5.txt'                           #MD5 of known "safe" file
 sha1_list_filepath = 'datasrc/sha1.txt'                         #SHA1 of known "safe" file
 sha256_list_filepath = 'datasrc/sha256.txt'                     #SHA256 of known "safe" file
